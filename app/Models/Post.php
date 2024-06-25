141f746eeb348aca718db4300580ac8087c43c25
alvaroperez0172022@gmail.com <andresfelipegutierrezruiz@gmail.com>
2024-06-25 17:11:17 -0500

    clase25

---
+++ app/Models/Post.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    const BOORADOR =1;
+    const PUBLICADO=2;
+
+    //relacion uno a muchos inversa
+    
+    public function user(){
+       return  $this->BelongsTo(User::class);
+    }
+    public function category(){
+       return $this->BelongsTo(Category::class);
+    }
+
+    //relacion muchos a muchos
+    
+    public function tags(){
+       return $this->BelongsToMany(Tag::class);
+    }
+
+    //Relacion uno a muchos polimorfica
+    public function images(){
+        return $this->morphMany(Image::class, 'imageable');
+    }
+}
